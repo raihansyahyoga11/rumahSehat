1ebbcf41bfcd0e3a82227c0706b5475c968c5a26
raihansyahyoga11 <raihansyahyoga@gmail.com>
2022-11-14 19:55:03 +0700

    feat: TK - Create 1/4 of model

---
+++ rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/RumahSehatApplication.java
@@ -0,0 +1,13 @@
+package TA_B_SYN_65.rumahSehat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RumahSehatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RumahSehatApplication.class, args);
+	}
+
+}
---
+++ rumahSehat/src/test/java/TA_B_SYN_65/rumahSehat/RumahSehatApplicationTests.java
@@ -0,0 +1,13 @@
+package TA_B_SYN_65.rumahSehat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RumahSehatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
