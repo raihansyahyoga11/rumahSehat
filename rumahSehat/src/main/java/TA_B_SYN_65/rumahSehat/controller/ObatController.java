f6c6c74f6b6eb42ce1d71ce5a935292cd185aa43
hilmi.albiruni <hilmi.albiruni@ui.ac.id>
2022-11-22 19:37:45 +0700

    membuat fitur 13 (belum berjalan)

---
+++ rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/controller/ObatController.java
@@ -0,0 +1,43 @@
+package TA_B_SYN_65.rumahSehat.controller;
+
+import TA_B_SYN_65.rumahSehat.model.ObatModel;
+import TA_B_SYN_65.rumahSehat.security.xml.Attributes;
+import TA_B_SYN_65.rumahSehat.security.xml.ServiceResponse;
+import TA_B_SYN_65.rumahSehat.service.ObatService;
+import TA_B_SYN_65.rumahSehat.service.UserService;
+import TA_B_SYN_65.rumahSehat.setting.Setting;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.security.Principal;
+import java.util.List;
+
+@Controller
+public class ObatController {
+//    Qualifier("obatServiceImpl")
+    @Autowired
+    private ObatService obatService;
+
+    @GetMapping("/obat/viewall")
+    public String listObat(Model model){
+        List<ObatModel> listObat = obatService.getListObat();
+        model.addAttribute("listObat", listObat);
+        return "viewall-obat";
+    }
+}
