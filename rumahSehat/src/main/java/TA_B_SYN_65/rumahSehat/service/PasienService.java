72a228e7e7861077a6861b56197ca7c4c56f97a8
Ahmad Aminullah Alfiyanto <ahmad.aminullah@ui.ac.id>
2022-11-28 07:25:56 +0700

    feat(7_web): lihat daftar appointment

--- rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/service/PasienService.java
+++ rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/service/PasienService.java
@@ -2,7 +2,9 @@ package TA_B_SYN_65.rumahSehat.service;
 
 import TA_B_SYN_65.rumahSehat.model.PasienModel;
 
-public interface PasienService {
+import java.util.List;
 
+public interface PasienService {
     PasienModel create(PasienModel pengguna);
+
 }
