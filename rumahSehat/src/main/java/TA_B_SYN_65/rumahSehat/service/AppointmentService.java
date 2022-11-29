25a2e9cc97c149b9d18b1e1d28ccb0f8d63c5c91
Alfina Azaria <alfina.azaria@ui.ac.id>
2022-11-29 15:12:39 +0700

    add main.dart file

--- rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/service/AppointmentService.java
+++ rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/service/AppointmentService.java
@@ -2,10 +2,6 @@ package TA_B_SYN_65.rumahSehat.service;
 
 import TA_B_SYN_65.rumahSehat.model.AppointmentModel;
 
-import java.util.List;
-
 public interface AppointmentService {
     AppointmentModel getAppointmentByKode(String kode);
-    List<AppointmentModel> getListAppointment();
-    void createAppointment(AppointmentModel appointment);
 }
