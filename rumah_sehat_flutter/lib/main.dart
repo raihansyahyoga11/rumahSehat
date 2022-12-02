d797c5e9a2a162501043d150dbed3b3089d0df9f
Alfina Azaria <alfina.azaria@ui.ac.id>
2022-12-02 15:24:24 +0700

    rewrite conflicted lines of code

--- rumah_sehat_flutter/lib/main.dart
+++ rumah_sehat_flutter/lib/main.dart
@@ -31,10 +31,10 @@ class _RumahSehatNavBarState extends State<RumahSehatNavBar> {
   int _selectedNavbar = 0;
   //navigasi page
   final screens=[
-    Center(child:Text('Home', style:TextStyle(fontSize: 60))),
+    HomePage(),
     Center(child:Text('Appointment', style:TextStyle(fontSize: 60))),
     Center(child:Text('Obat', style:TextStyle(fontSize: 60))),
-    PofilePage(),
+    ProfilePage(),
   ];
 
   void _changeSelectedNavBar(int index) {
@@ -77,8 +77,4 @@ class _RumahSehatNavBarState extends State<RumahSehatNavBar> {
       ),
     );
   }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> 2f469ab0a706a1c92a47390abf43f702bc689e8d
