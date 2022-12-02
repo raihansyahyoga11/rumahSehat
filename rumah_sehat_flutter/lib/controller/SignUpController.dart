47848d1822d110e55f5b2c257c1e50c1548581c5
Alfina Azaria <alfina.azaria@ui.ac.id>
2022-12-02 22:34:58 +0700

    feat: finish signup pasien

--- rumah_sehat_flutter/lib/controller/SignUpController.dart
+++ rumah_sehat_flutter/lib/controller/SignUpController.dart
@@ -11,9 +11,10 @@ class SignUpController {
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController umurController = TextEditingController();
+  final TextEditingController roleController = TextEditingController();
 
   // Versi Yoga
-  Future<int> attemptSignUp(String username, String password, String umur, String name, String email) async {
+  Future<int> attemptSignUp(String username, String password, String name, String email, String umur, String role) async {
     const url = 'http://localhost:8080/api/mobile/signupPasien';
     print("haha");
     var response = await http.post(Uri.parse(url),
@@ -24,17 +25,17 @@ class SignUpController {
           "nama" : nameController.text,
           "email" : emailController.text,
           "umur": umurController.text,
-
+          "role": roleController.text
         }));
     print("huhe");
     if (response.statusCode == 200) {
       print("masuk 200");
       var loginMaterial = json.decode(response.body);
-      var token = loginMaterial['token'];
+      // var token = loginMaterial['token'];
       SharedPreferences prefrences = await SharedPreferences.getInstance();
 
 
-      prefrences.setString('token', loginMaterial['token']);
+      // prefrences.setString('token', loginMaterial['token']);
       prefrences.setString('username', loginMaterial['username']);
 
       return response.statusCode;
@@ -43,40 +44,13 @@ class SignUpController {
     }
   }
 
-  logOut() async {
-    SharedPreferences prefrences = await SharedPreferences.getInstance();
-    prefrences.remove("token");
-    token = null;
-    print(token);
-    prefrences.remove("token");
-  }
-
-  // Versi Docs Flutter
-  // Future<http.Response> attemptSignUp(String username, String password, String umur, String name, String email) async {
-  //   final response = await http.post(
-  //     Uri.parse('http://localhost:8080/api/mobile/signup'),
-  //     headers: <String, String>{
-  //       'Content-Type': 'application/json; charset=UTF-8',
-  //     },
-  //     body: jsonEncode(<String, String>{
-  //       "username": usernameController.text,
-  //       "password": passwordController.text,
-  //       "nama" : nameController.text,
-  //       "email" : emailController.text,
-  //       "umur": umurController.text,
-  //     }),
-  //   );
-  //
-  //   if (response.statusCode == 200) {
-  //     // If the server did return a 201 CREATED response,
-  //     // then parse the JSON.
-  //     print("masuk 200");
-  //     return jsonDecode(response.body);
-  //   } else {
-  //     // If the server did not return a 201 CREATED response,
-  //     // then throw an exception.
-  //     throw Exception('Failed to create pasien.');
-  //   }
-  }
+  // logOut() async {
+  //   SharedPreferences prefrences = await SharedPreferences.getInstance();
+  //   prefrences.remove("token");
+  //   token = null;
+  //   print(token);
+  //   prefrences.remove("token");
+  // }
+}
 
 
