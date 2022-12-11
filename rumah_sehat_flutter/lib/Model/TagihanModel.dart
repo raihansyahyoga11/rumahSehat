e79930e36a267699cee6ffc2caa9b8b2f59d9594
M Arieksha <m.arieksha@gmail.com>
2022-12-11 22:40:17 +0700

    update pubspec yaml version

--- rumah_sehat_flutter/lib/Model/TagihanModel.dart
+++ rumah_sehat_flutter/lib/Model/TagihanModel.dart
@@ -5,9 +5,6 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:http/http.dart' as http;
 import 'package:json_annotation/json_annotation.dart';
 
-
-
-
 class TagihanModel {
   final String kode;
   final LocalDateTimeInputElement tanggalTerbuat;
@@ -16,8 +13,6 @@ class TagihanModel {
   final int jumlahTagihan;
   final List<dynamic> listAppointment;
 
-
-
   TagihanModel({
     required this.kode,
     required this.tanggalTerbuat,
@@ -33,27 +28,6 @@ class TagihanModel {
         tanggalBayar: json['tanggal_bayar'],
         isPaid: json['is_paid'],
         jumlahTagihan: json['jumlah_tagihan'],
-        listAppointment: json['listAppointment']
-    );
-  }
-}
-
-  TagihanModel({
-    required this.kode,
-    required this.tanggalTerbuat,
-    required this.tanggalBayar,
-    required this.isPaid,
-    required this.jumlahTagihan,
-    required this.listAppointment,
-  });
-  factory TagihanModel.fromJson(Map<String, dynamic> json) {
-    return TagihanModel(
-        kode: json['kode'],
-        tanggalTerbuat: json['tanggal_terbuat'],
-        tanggalBayar: json['tanggal_bayar'],
-        isPaid: json['is_paid'],
-        jumlahTagihan: json['jumlah_tagihan'],
-        listAppointment: json['listAppointment']
-    );
+        listAppointment: json['listAppointment']);
   }
 }
