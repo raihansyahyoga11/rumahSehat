122ed27f2a757754ca801d3c49c258b36083acf8
Raihansyah Yoga Adhitama <raihansyah.yoga@ui.ac.id>
2022-12-12 09:54:59 +0700

    Merge barchart into main

--- rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/service/BarChartServiceImpl.java
+++ rumahSehat/src/main/java/TA_B_SYN_65/rumahSehat/service/BarChartServiceImpl.java
@@ -1,11 +1,16 @@
 package TA_B_SYN_65.rumahSehat.service;
 
 import TA_B_SYN_65.rumahSehat.model.BarChartModel;
+import TA_B_SYN_65.rumahSehat.model.JumlahModel;
+import TA_B_SYN_65.rumahSehat.model.ObatModel;
 import TA_B_SYN_65.rumahSehat.repository.BarChartDb;
+import TA_B_SYN_65.rumahSehat.repository.JumlahDb;
+import TA_B_SYN_65.rumahSehat.repository.ObatDb;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 @Service
 @Transactional
@@ -14,8 +19,21 @@ public class BarChartServiceImpl implements BarChartService {
     @Autowired
     BarChartDb barChartDb;
 
+    @Autowired
+    ObatService obatService;
+
+    @Autowired
+    JumlahDb jumlahdb;
+
     public void addBarChart(BarChartModel barChart) {
         barChartDb.save(barChart);
-    };
+    }
+
+    ;
+
+
+
 
 }
+
+
